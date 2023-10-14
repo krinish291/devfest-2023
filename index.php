ee343b2d175eca2cc2d2c25e894805cb9a7bbdf9
Sarthak <notsarthakshah@gmail.com>
2023-10-14 17:11:02 +0530

    ticket icon in navbar

--- index.php
+++ index.php
@@ -91,13 +91,9 @@ TODO (@knightkill)
 			
 			<a href="#ticket-wrap" class="ticket-link">
 				<div class="ticket">
-					<p>157 seats left</p>
+					<p>Secure Seats!</p>
 					<div class="pulse">
-						<svg version="1.1" id="svg-ticket" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-						width="36px" height="36px" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve">
-							<path fill="none" stroke="#FFFFFF" stroke-width="4" stroke-miterlimit="10" d="M1,21c0,20,31,38,31,38s31-18,31-38
-										c0-8.285-6-16-15-16c-8.285,0-16,5.715-16,14c0-8.285-7.715-14-16-14C7,5,1,12.715,1,21z"/>					
-						</svg>
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"><path fill="currentColor" d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5V6a.5.5 0 0 1-.5.5a1.5 1.5 0 0 0 0 3a.5.5 0 0 1 .5.5v1.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 11.5V10a.5.5 0 0 1 .5-.5a1.5 1.5 0 1 0 0-3A.5.5 0 0 1 0 6V4.5ZM1.5 4a.5.5 0 0 0-.5.5v1.05a2.5 2.5 0 0 1 0 4.9v1.05a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-1.05a2.5 2.5 0 0 1 0-4.9V4.5a.5.5 0 0 0-.5-.5h-13Z"/></svg>
 					</div>
 				</div>
 			</a>
