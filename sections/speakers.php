527702fea6a54c557866e93a435a3c02b8dcc676
Hardip Patel <hardip@anormaly.com>
2023-10-23 21:35:15 +0530

    Refactor speaker section and add single speaker component

--- sections/speakers.php
+++ sections/speakers.php
@@ -30,21 +30,8 @@ TODO (@krinish)
                             <h4>To be announced soon</h4>
                             <div class="title-page-line mt-4 mb-5"></div>
                         </div>
-                        <!-- <div class="col-md-3">
-                            <div class="speaker-image">
-                                <h6>Krinish Radadiya</h6>
-                                <img src="https://avatars.githubusercontent.com/u/47221638?v=4" alt="">
-                            </div>
-                        </div>
-                        <div class="col-md-9 mt-4 mt-md-0">
-                            <h5>Engineering Manager</h5>
-                            <p><em>AtliQ Technologies, Vadodara</em></p>
-                            <p class="mt-4">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit.</p>
-                            <div class="social-team mt-4">
-                                <a href="#">twitter</a>
-                                <a href="#">linkedin</a>
-                            </div>
-                        </div> -->
+
+                        <?php /*include 'templates/components/single_speaker.php'; */?>
                     </div>
                 </div>
             </div>
